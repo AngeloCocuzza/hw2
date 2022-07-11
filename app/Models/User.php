a13980056187e89067ddaff97a00e4b847a92cc4
AngeloCocuzza <102428825+AngeloCocuzza@users.noreply.github.com>
2022-07-11 11:27:12 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,11 +11,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+   
     
     protected $fillable = [
         'email',
@@ -24,26 +20,11 @@ class User extends Authenticatable
         'lastname'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    
 
     public $timestamps=false;
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+   
 
     public function carrello() {
         return $this->hasMany("App\Models\Carrello");
